6bf8c9f121e51d5849c6a341f25224d3b7ff805c
serotoninmix <mrspectrumdesigns@gmail.com>
2023-11-04 16:56:28 +0100

    sq

---
+++ GNOSIS/src/views/mac-book-air2.js
@@ -0,0 +1,200 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './mac-book-air2.css'
+
+const MacBookAir2 = (props) => {
+  return (
+    <div className="mac-book-air2-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="mac-book-air2-mac-book-air2">
+        <img
+          alt="Rectangle11439"
+          src="/external/rectangle11439-6daj-200h.png"
+          className="mac-book-air2-rectangle1"
+        />
+        <img
+          alt="Rectangle21439"
+          src="/external/rectangle21439-bpms-200h.png"
+          className="mac-book-air2-rectangle2"
+        />
+        <span className="mac-book-air2-text">
+          <span>A. Say &quot;Hi!&quot; to Mr. Plastic Bag.</span>
+        </span>
+        <img
+          alt="Rectangle31439"
+          src="/external/rectangle31439-jjqb-200h.png"
+          className="mac-book-air2-rectangle3"
+        />
+        <span className="mac-book-air2-text2">
+          <span>
+            You&apos;re at the entrance of the magical grocery forest. Do you:
+          </span>
+        </span>
+        <span className="mac-book-air2-text4">
+          <span>LEVEL 1: Grocery Grab</span>
+        </span>
+        <div className="mac-book-air2-tree031">
+          <div className="mac-book-air2-group47">
+            <img
+              alt="Rectangle2481034"
+              src="/external/rectangle2481034-989w.svg"
+              className="mac-book-air2-rectangle248"
+            />
+            <img
+              alt="Rectangle2911034"
+              src="/external/rectangle2911034-s6qk.svg"
+              className="mac-book-air2-rectangle291"
+            />
+          </div>
+          <div className="mac-book-air2-group49">
+            <div className="mac-book-air2-group48">
+              <img
+                alt="Union1034"
+                src="/external/union1034-om.svg"
+                className="mac-book-air2-union"
+              />
+              <img
+                alt="Union1034"
+                src="/external/union1034-6fyo.svg"
+                className="mac-book-air2-union01"
+              />
+              <img
+                alt="Union1034"
+                src="/external/union1034-vcbi.svg"
+                className="mac-book-air2-union02"
+              />
+              <img
+                alt="Union1034"
+                src="/external/union1034-g7rp.svg"
+                className="mac-book-air2-union03"
+              />
+              <img
+                alt="Union1034"
+                src="/external/union1034-sv2b.svg"
+                className="mac-book-air2-union04"
+              />
+              <img
+                alt="Union1034"
+                src="/external/union1034-g5vt.svg"
+                className="mac-book-air2-union05"
+              />
+            </div>
+            <img
+              alt="Subtract1034"
+              src="/external/subtract1034-wgg.svg"
+              className="mac-book-air2-subtract"
+            />
+          </div>
+        </div>
+        <div className="mac-book-air2-tree0311">
+          <div className="mac-book-air2-group471">
+            <img
+              alt="Rectangle248104"
+              src="/external/rectangle248104-cdf9.svg"
+              className="mac-book-air2-rectangle2481"
+            />
+            <img
+              alt="Rectangle291105"
+              src="/external/rectangle291105-xmq.svg"
+              className="mac-book-air2-rectangle2911"
+            />
+          </div>
+          <div className="mac-book-air2-group491">
+            <div className="mac-book-air2-group481">
+              <img
+                alt="Union108"
+                src="/external/union108-ccyt.svg"
+                className="mac-book-air2-union06"
+              />
+              <img
+                alt="Union1012"
+                src="/external/union1012-nbv.svg"
+                className="mac-book-air2-union07"
+              />
+              <img
+                alt="Union1016"
+                src="/external/union1016-qwwm.svg"
+                className="mac-book-air2-union08"
+              />
+              <img
+                alt="Union1020"
+                src="/external/union1020-95r.svg"
+                className="mac-book-air2-union09"
+              />
+              <img
+                alt="Union1024"
+                src="/external/union1024-yc3.svg"
+                className="mac-book-air2-union10"
+              />
+              <img
+                alt="Union1028"
+                src="/external/union1028-wc2r.svg"
+                className="mac-book-air2-union11"
+              />
+            </div>
+            <img
+              alt="Subtract1032"
+              src="/external/subtract1032-w77u.svg"
+              className="mac-book-air2-subtract1"
+            />
+          </div>
+        </div>
+        <div className="mac-book-air2-tree061">
+          <div className="mac-book-air2-group62">
+            <img
+              alt="Rectangle2481034"
+              src="/external/rectangle2481034-vwzn.svg"
+              className="mac-book-air2-rectangle2482"
+            />
+            <img
+              alt="Rectangle2911034"
+              src="/external/rectangle2911034-gsg.svg"
+              className="mac-book-air2-rectangle2912"
+            />
+          </div>
+          <img
+            alt="Vector81034"
+            src="/external/vector81034-1rt.svg"
+            className="mac-book-air2-vector8"
+          />
+          <img
+            alt="Intersect1034"
+            src="/external/intersect1034-fg29.svg"
+            className="mac-book-air2-intersect"
+          />
+          <img
+            alt="Vector91034"
+            src="/external/vector91034-wkfu.svg"
+            className="mac-book-air2-vector9"
+          />
+          <img
+            alt="Intersect1034"
+            src="/external/intersect1034-0h5.svg"
+            className="mac-book-air2-intersect1"
+          />
+          <img
+            alt="Vector101034"
+            src="/external/vector101034-epg.svg"
+            className="mac-book-air2-vector10"
+          />
+          <img
+            alt="Intersect1034"
+            src="/external/intersect1034-veem.svg"
+            className="mac-book-air2-intersect2"
+          />
+          <img
+            alt="Vector111034"
+            src="/external/vector111034-rla.svg"
+            className="mac-book-air2-vector11"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MacBookAir2
